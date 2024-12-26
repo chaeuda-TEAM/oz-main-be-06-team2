8c1cd6eda136e5cbe62bfec173a4261ebdb3de9f
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-12-26 19:25:25 +0900

    Chore : products모델 verbose_name 추가(#47)

--- django/a_apis/models/products.py
+++ django/a_apis/models/products.py
@@ -6,37 +6,49 @@ from django.db import models
 
 
 class Cost(CommonModel):
-    cost_id = models.IntegerField(primary_key=True)
-    cost_type = models.CharField(max_length=36)
-    mg_cost = models.IntegerField()
+    cost_id = models.IntegerField(primary_key=True, verbose_name="관리비용 ID")
+    cost_type = models.CharField(max_length=36, verbose_name="관리비명")
+    mg_cost = models.IntegerField(verbose_name="관리비용")
 
     class Meta:
         db_table = "cost"
         verbose_name = "관리비용"
+        verbose_name_plural = "관리비용"
 
     def __str__(self):
         return self.cost_type
 
 
 class ProductAddress(CommonModel):
-    address_id = models.IntegerField(primary_key=True)
-    add_new = models.CharField(max_length=255)
-    add_old = models.CharField(max_length=255)
-    latitude = models.DecimalField(max_digits=10, decimal_places=6)
-    longitude = models.DecimalField(max_digits=10, decimal_places=6)
+    address_id = models.IntegerField(primary_key=True, verbose_name="매물주소 ID")
+    add_new = models.CharField(max_length=255, verbose_name="도로명주소")
+    add_old = models.CharField(max_length=255, verbose_name="구주소")
+    latitude = models.DecimalField(max_digits=10, decimal_places=6, verbose_name="위도")
+    longitude = models.DecimalField(
+        max_digits=10, decimal_places=6, verbose_name="경도"
+    )
 
     class Meta:
         db_table = "product_address"
-        verbose_name = "주소"
+        verbose_name = "매물주소"
+        verbose_name_plural = "매물주소"
 
     def __str__(self):
         return self.add_new
 
 
 class ProductContents(CommonModel):
-    contents = models.IntegerField(primary_key=True)
-    img_url = models.FileField(upload_to="img/")
-    video_url = models.FileField(upload_to="video/")
+    contents_id = models.IntegerField(primary_key=True, verbose_name="이미지/동영상 ID")
+    img_url = models.FileField(upload_to="img/", verbose_name="이미지 URL")
+    video_url = models.FileField(upload_to="video/", verbose_name="동영상 URL")
+
+    class Meta:
+        db_table = "product_contents"
+        verbose_name = "이미지/동영상"
+        verbose_name_plural = "이미지/동영상"
+
+    def __str__(self):
+        return self.img_url
 
 
 class ProductDetail(CommonModel):
@@ -70,16 +82,18 @@ class ProductDetail(CommonModel):
     pro_floor = models.IntegerField(verbose_name="층")
     pro_intro = models.TextField(verbose_name="상세설명")
     sale = models.BooleanField(default=True, verbose_name="판매여부")
-    cost_id = models.ForeignKey(Cost, on_delete=models.CASCADE, verbose_name="관리비용")
+    cost_id = models.ForeignKey(
+        Cost, on_delete=models.CASCADE, verbose_name="관리비용 ID"
+    )
     address_id = models.ForeignKey(
-        ProductAddress, on_delete=models.CASCADE, verbose_name="주소"
+        ProductAddress, on_delete=models.CASCADE, verbose_name="주소 ID"
     )
     contents = models.ForeignKey(
-        ProductContents, on_delete=models.CASCADE, verbose_name="내용"
+        ProductContents, on_delete=models.CASCADE, verbose_name="내용 ID"
     )
 
     class Meta:
-        db_table = "products"
+        db_table = "product_detail"
         verbose_name = "매물정보"
         verbose_name_plural = "매물정보목록"
 
