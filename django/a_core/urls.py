2d179038c4f1c44a7164a8c753b5bf851d9759cc
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-12-26 23:52:42 +0900

    Chore : urlpatterns static code 추가(#51)

--- django/a_core/urls.py
+++ django/a_core/urls.py
@@ -1,5 +1,7 @@
 from a_apis.api.api import api
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
+
+# If DEBUG is True, serve static files in development environment.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
