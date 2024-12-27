52efdbd938743bfdf3c42a3eed40dd8488b3b590
bamjun <khy5116@naver.com>
2024-12-27 12:41:28 +0900

    merge from user

--- django/a_user/admin.py
+++ django/a_user/admin.py
@@ -7,13 +7,12 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 class CustomUserAdmin(BaseUserAdmin):
     list_display = (
         "id",
-        "user_id",
         "username",
         "phone_number",
         "email",
         "is_email_verified",
     )
-    search_fields = ("user_id", "username", "email", "phone_number")
+    search_fields = ("username", "email", "phone_number")
     ordering = ("id",)
 
 
