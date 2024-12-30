7131bfff121cc9a0b4e637952dff75ac3c0da2d7
bamjun <khy5116@naver.com>
2024-12-30 15:13:35 +0900

    refactor: Enhance social login implementation and code formatting(#4 #54)
    - Improve social login services (Google, Kakao, Naver)
    - Add social user creation logic
    - Update environment variables for social auth
    - Format code structure and indentation

--- django/a_apis/admin.py
+++ django/a_apis/admin.py
@@ -1,5 +1,11 @@
 from a_apis.models.email_verification import EmailVerification
-from a_apis.models.products import Cost, ProductAddress, ProductDetail, ProductImg, ProductVideo
+from a_apis.models.products import (
+    Cost,
+    ProductAddress,
+    ProductDetail,
+    ProductImg,
+    ProductVideo,
+)
 
 from django.contrib import admin
 
@@ -27,11 +33,13 @@ class ProductImgAdmin(admin.ModelAdmin):
     list_display = ("img_id", "img_url")
     search_fields = ("img_id",)
 
+
 # ProductVideo 관리자 클래스
 @admin.register(ProductVideo)
 class ProductVideoAdmin(admin.ModelAdmin):
     list_display = ("video_id", "video_url")
     search_fields = ("video_id",)
+
 
 # ProductDetail 관리자 클래스
 @admin.register(ProductDetail)
@@ -46,4 +54,8 @@ class ProductDetailAdmin(admin.ModelAdmin):
     )
     list_filter = ("pro_type", "pro_heat", "sale")
     search_fields = ("pro_title", "pro_intro")
-    raw_id_fields = ("user_no", "cost_id", "address_id",)
+    raw_id_fields = (
+        "user_no",
+        "cost_id",
+        "address_id",
+    )
