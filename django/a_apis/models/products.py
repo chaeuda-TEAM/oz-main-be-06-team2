337104c0f49999b8c0a3717b872dc1e53b1e1b36
BE02LEEJAEHOON <ljhx6787@naver.com>
2024-12-30 17:05:30 +0900

    Chore : 매물테이블수정(#66)

--- django/a_apis/models/products.py
+++ django/a_apis/models/products.py
@@ -5,20 +5,6 @@ from django.conf import settings
 from django.db import models
 
 
-class Cost(CommonModel):
-    id = models.AutoField(primary_key=True, verbose_name="관리비용 ID")
-    cost_type = models.CharField(max_length=36, verbose_name="관리비명")
-    mg_cost = models.IntegerField(verbose_name="관리비용")
-
-    class Meta:
-        db_table = "cost"
-        verbose_name = "관리비용"
-        verbose_name_plural = "관리비용"
-
-    def __str__(self):
-        return self.cost_type
-
-
 class ProductAddress(CommonModel):
     id = models.AutoField(primary_key=True, verbose_name="매물주소 ID")
     add_new = models.CharField(max_length=255, verbose_name="도로명주소")
@@ -31,6 +17,7 @@ class ProductAddress(CommonModel):
     class Meta:
         db_table = "product_address"
         verbose_name = "주소"
+        verbose_name_plural = "주소"
 
     def __str__(self):
         return self.add_new
@@ -45,6 +32,7 @@ class ProductVideo(CommonModel):
     class Meta:
         db_table = "product_video"
         verbose_name = "동영상"
+        verbose_name_plural = "동영상"
 
     def __str__(self):
         return self.video_url.url
@@ -54,7 +42,7 @@ class ProductDetail(CommonModel):
     HEAT_CHOICES = [
         ("gas", "가스보일러"),
         ("oil", "기름보일러"),
-        ("briquet", "연탄보일러"),
+        ("briquette", "연탄보일러"),
         ("heat_etc", "기타"),
     ]
     TYPE_CHOICES = [
@@ -66,8 +54,9 @@ class ProductDetail(CommonModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="유저 ID")
     pro_title = models.CharField(max_length=50, verbose_name="제목")
     pro_price = models.IntegerField(verbose_name="매물금액")
+    management_cost = models.IntegerField(verbose_name="관리비", null=True, blank=True)
     pro_supply_a = models.DecimalField(
-        max_digits=10, decimal_places=2, verbose_name="공급면적"
+        max_digits=10, decimal_places=2, verbose_name="공급면적(평수)"
     )
     pro_site_a = models.DecimalField(
         max_digits=10, decimal_places=2, verbose_name="부지면적"
@@ -78,12 +67,14 @@ class ProductDetail(CommonModel):
     pro_type = models.CharField(
         max_length=10, choices=TYPE_CHOICES, verbose_name="건물유형"
     )
-    pro_floor = models.CharField(max_length=10, verbose_name="층")
-    pro_intro = models.TextField(verbose_name="상세설명")
+    pro_floor = models.IntegerField(verbose_name="층")
+    description = models.TextField(verbose_name="상세설명")
     sale = models.BooleanField(default=True, verbose_name="판매여부")
-    cost = models.ForeignKey(Cost, on_delete=models.CASCADE, verbose_name="관리비용 ID")
     address = models.ForeignKey(
-        ProductAddress, on_delete=models.CASCADE, verbose_name="주소 ID"
+        ProductAddress,
+        on_delete=models.CASCADE,
+        verbose_name="주소 ID",
+        related_name="product_address",
     )
     video = models.ForeignKey(
         ProductVideo,
@@ -91,12 +82,13 @@ class ProductDetail(CommonModel):
         verbose_name="동영상 ID",
         null=True,
         blank=True,
+        related_name="product_video",
     )
 
     class Meta:
         db_table = "product_detail"
         verbose_name = "매물정보"
-        verbose_name_plural = "매물정보목록"
+        verbose_name_plural = "매물정보"
 
     def __str__(self):
         return self.pro_title
@@ -106,12 +98,16 @@ class ProductImg(CommonModel):
     id = models.AutoField(primary_key=True, verbose_name="이미지 ID")
     img_url = models.FileField(upload_to="img/", verbose_name="이미지 URL")
     product_detail = models.ForeignKey(
-        ProductDetail, on_delete=models.CASCADE, verbose_name="상품 ID"
+        ProductDetail,
+        on_delete=models.CASCADE,
+        verbose_name="상품 ID",
+        related_name="product_images",
     )
 
     class Meta:
         db_table = "product_img"
-        verbose_name = "이미지"
+        verbose_name = ("이미지",)
+        verbose_name_plural = "이미지"
 
     def __str__(self):
         return self.img_url.url
