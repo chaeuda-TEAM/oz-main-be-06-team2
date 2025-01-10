aa8a3cbdb5bbdb355fb7dfda353fea033b976551
bamjun <khy5116@naver.com>
2025-01-10 14:45:04 +0900

    middle save feat: Update dependencies and enhance email validation
    - Added `dnspython` and `email-validator` packages to `poetry.lock` for improved DNS handling and email validation.
    - Updated `pydantic` version to `2.10.4` and adjusted its usage in `users.py` to utilize `EmailStr` for email fields.
    - Implemented model validators in `EmailVerificationSchema` to check for duplicate fields and validate the length of the code.
    - Updated `pyproject.toml` to reflect the new `pydantic` dependency with email extras.

--- django/a_apis/schema/users.py
+++ django/a_apis/schema/users.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from ninja import Schema
+from pydantic import EmailStr, Extra, field_validator, model_validator
 
 
 class EmailVerificationRequestSchema(Schema):
@@ -57,8 +58,29 @@ class WithdrawalSchema(Schema):
 
 
 class EmailVerificationSchema(Schema):
-    email: str
+    email: EmailStr
     code: str
+
+    @model_validator(mode="before")
+    @classmethod
+    def check_duplicate_fields(cls, data):
+        print(f"{type(data) = }")
+        print(f"{data = }")
+        print(f"{data.email = }")
+        if isinstance(data, dict):
+            email_count = sum(1 for k in data.keys() if k == "email")
+            code_count = sum(1 for k in data.keys() if k == "code")
+
+            if email_count > 1 or code_count > 1:
+                raise ValueError("중복된 필드가 존재합니다")
+        return data
+
+    @field_validator("code")
+    @classmethod
+    def validate_code(cls, v):
+        if not v or len(v) != 6:
+            raise ValueError("유효하지 않은 인증코드입니다 (길이가 6자여야 함)")
+        return v
 
 
 class LogoutSchema(Schema):
